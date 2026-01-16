49a1d644aa618a566e02f90f525af5c6960199f1
juliemeta <juliemonefeldt_404@hotmail.com>
2026-01-16 10:57:40 +0100

    Created Dictionary component and added footer to App.jsx

--- dictionary/src/App.jsx
+++ dictionary/src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App text-center">
-      <h1>Hello from App.jsx</h1>
-      <button className="btn btn-danger">hey! Bootstrap working</button>
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer className="text-center">
+        <div>~ Coded by Julie M. Jeffries ~</div>
+        <div>
+          Project is open-sourced on{" "}
+          <a href="https://github.com/juliemeta/dictionary">GitHub</a>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ dictionary/src/Dictionary.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return (
+    <div className="Dictionary">
+      <h1 className="text-center">Dictionary</h1>
+    </div>
+  );
+}
