4d560175abd145a9bbbd9812b04b93e6231dcda1
juliemeta <juliemonefeldt_404@hotmail.com>
2026-01-16 10:12:08 +0100

    Initial React project setup with Bootstrap

---
+++ dictionary/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App text-center">
+      <h1>Hello from App.jsx</h1>
+      <button className="btn btn-danger">hey! Bootstrap working</button>
+    </div>
+  );
+}
