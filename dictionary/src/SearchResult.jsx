b919a4a402b22532a8a74d7b1770f16c2ab229f9
juliemeta <juliemonefeldt_404@hotmail.com>
2026-01-21 13:35:23 +0100

    Final touches

--- dictionary/src/SearchResult.jsx
+++ dictionary/src/SearchResult.jsx
@@ -31,12 +31,11 @@ export default function SearchResult(props) {
         {props.searchResult.meanings?.map((meaning, index) => (
           <div key={index}>
             <div className="definitions-division shadow">
+              <p className="part-of-speech text-capitalize">
+                <strong>{meaning.partOfSpeech}</strong>
+              </p>
               <h3 className="keyword">{props.searchResult.word}</h3>
               <p className="phonetics">/{props.searchResult.phonetic}/</p>
-              <p className="part-of-speech fst-italic">
-                {" "}
-                {meaning.partOfSpeech}
-              </p>
 
               <p className="definition">
                 <strong>Definition</strong> <br />
