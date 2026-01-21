c9040266caa2c6751e83941299117f73de6a67be
juliemeta <juliemonefeldt_404@hotmail.com>
2026-01-21 10:33:20 +0100

    In search() changed let > const and made the Pexels API functional

--- dictionary/src/Dictionary.jsx
+++ dictionary/src/Dictionary.jsx
@@ -6,21 +6,31 @@ import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [searchResult, setSearchResult] = useState(null);
-  let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setSearchResult(response.data);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
   }
 
   function search() {
     if (!keyword.trim()) return;
-    let apiKey = "c2t7ea4432f52e0o6d402fd54c5bc269";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    const apiKey = "c2t7ea4432f52e0o6d402fd54c5bc269";
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleDictionaryResponse);
 
-    let pexelsApiKey =
+    const pexelsApiKey =
       "WtfXQhN4zMNDB5wNhkpemrAxlLpHgLYQy1bnOyQ7HUtMy7H6QXpoJWs9";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
+    axios
+      .get(`${pexelsApiUrl}`, {
+        headers: {
+          Authorization: `${pexelsApiKey}`,
+        },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -32,34 +42,24 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
 
-  function load() {
-    setLoaded(true);
-    search();
-  }
-
-  if (loaded) {
-    return (
-      <div className="Dictionary">
-        <h1 className="text-center">Dictionary</h1>
-        <h2 className="subheading text-center">
-          Which word do you want to look up?
-        </h2>
-        <form className="search-form" onSubmit={handleSubmit}>
-          <input
-            className="search-bar shadow"
-            type="search"
-            onChange={handleKeywordChange}
-            placeholder="Type keyword..."
-          />
-          <button className="btn search-button shadow">Search</button>
-        </form>
-        {searchResult && (
-          <SearchResult searchResult={searchResult} keyword={keyword} />
-        )}
-      </div>
-    );
-  } else {
-    load();
-    return "Loading";
-  }
+  return (
+    <div className="Dictionary">
+      <h1 className="text-center">Dictionary</h1>
+      <h2 className="subheading text-center">
+        Which word do you want to look up?
+      </h2>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          className="search-bar shadow"
+          type="search"
+          onChange={handleKeywordChange}
+          placeholder="Type keyword..."
+        />
+        <button className="btn search-button shadow">Search</button>
+      </form>
+      {searchResult && (
+        <SearchResult searchResult={searchResult} keyword={keyword} />
+      )}
+    </div>
+  );
 }
